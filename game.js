126407aa008b08406ea24107ee214f275e158435
Trey Kemp <kemptrey@outlook.com>
2021-06-04 08:48:03 -0600

    added correct incorrect effect

--- game.js
+++ game.js
@@ -95,7 +95,15 @@ choices.forEach((choice) => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    getNewQuestion();
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    console.log(classToApply);
+
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
