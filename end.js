64c61126c28aef3c00891f8a855963f6326a3683
Trey Kemp <kemptrey@outlook.com>
2021-06-07 10:50:49 -0600

    end page styled and working

---
+++ end.js
@@ -0,0 +1,12 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+  e.preventDefault();
+};
