9ca70e5fccb2fc272a5040802653ee6171179849
Trey Kemp <kemptrey@outlook.com>
2021-06-07 11:32:03 -0600

    added score function

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
@@ -9,4 +12,17 @@ username.addEventListener("keyup", () => {
 
 saveHighScore = (e) => {
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value,
+  };
+  highScores.push(score);
+
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
+  console.log(highScores);
 };
